9f6849448e8b2504e1ac76e31c9631cdfe55f712
SamreenAsif <l201292@lhr.nu.edu.pk>
2023-04-01 10:50:30 +0500

    wishList ver3

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,14 +1,36 @@
 import React from "react";
 import "./ListItem.css";
+import movetoTopIcon from "./imgs/movetoTopIcon.png";
 
-export const ListItem = ({name, onDelete}) => {
+export const ListItem = (props) => {
+  const moveToTop = () => {
+    props.onMoveToTop(props.index);
+  };
   return (
     <>
       <div className="list-item-container">
-        <li className="list-item">{name}</li>
-        <button id="delBtn" onClick={onDelete}>Delete</button>
+        <li className="list-item">{props.name}</li>
+        <div className="btns-container">
+          <label
+            htmlFor="task-priority"
+            className={`label ${
+              props.priority === "High"
+                ? "high-priority"
+                : props.priority === "Medium"
+                ? "medium-priority"
+                : "low-priority"
+            }`}
+          >
+            {props.priority}
+          </label>
+          <button id="delBtn" onClick={props.onDelete}>
+            Delete
+          </button>
+          <button class="moveToTopBtn" onClick={moveToTop}>
+            <img class="image" src={movetoTopIcon} alt="Move to Top" />
+          </button>
+        </div>
       </div>
-
     </>
   );
 };
